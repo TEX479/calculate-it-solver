bb503946953ec80ac6bd36cb48d324ca3ede395e
TEX_479 <tex_gaming@web.de>
2024-09-24 21:55:22 +0100

    add buttons
    buttons:
    - "->25" changes current number to 25
    - "cut" cuts the first digit of target number, if it has more than one digit

--- moveFinder.py
+++ moveFinder.py
@@ -5,13 +5,13 @@ from sympy import isprime, prevprime, nextprime # type: ignore
 
 
 digits: set[str] = {'0','1','2','3','4','5','6','7','8','9'}
-operations: set[str] = {'add','sub','mul','div','mod','sq','sqr','swap','primes','X++','reverse','near'}
+operations: set[str] = {'add','sub','mul','div','mod','sq','sqr','swap','primes','X++','reverse','near','->25','cut'}
 operations_with_argument : set[str] = {'add', 'sub', 'mul', 'div', 'mod'}
 buttons_implemented: set[str] =  digits | operations | {'='}
 button_costs_default: dict[str, float] = {'0': 1.0, '1': 1.0, '2': 1.0, '3': 1.0, '4': 1.0, '5': 1.0, '6': 1.0, '7': 1.0, '8': 1.0, '9': 1.0,
                                           'add': 1.1, 'sub': 1.1, 'mul': 1.1, 'div': 1.1,
                                           'mod': 1.1, 'sq': 1.0, 'sqr': 1.0,
-                                          'swap': 0.8, 'primes': 0.8, 'X++': 1.0, 'reverse':0.9, 'near': 0.9,
+                                          'swap': 0.8, 'primes': 0.8, 'X++': 1.0, 'reverse':0.9, 'near': 2.0, '->25': 1.2, 'cut': 1.0,
                                           '=': 0}
 
 def _find_nearest_prime(number_current: int) -> int:
@@ -124,6 +124,11 @@ def check_button_sequence(button_sequence:list[str], buttons_availible:list[str]
                     distance = abs(number_target - number_current) # type: ignore
                     distance = min(distance, 10)
                     number_current += distance * direction
+                case '->25':
+                    number_current = 25
+                case 'cut':
+                    if len(str(number_target)) < 2: return None # <- the game does not cut the first digit, if it is the only one
+                    number_target = int(str(number_target)[1:])
                 case a    : 
                     warning = f"unknown operation '{a}'. how did that slip through the validation of inputs?"
                     ic(warning)
